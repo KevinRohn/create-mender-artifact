94def136c9d98c5a93471233ac4b615d3ebaf9a2
KevinR <kvn.rhn@gmail.com>
2021-12-07 18:02:19 +0100

    test mender artifact

--- entrypoint.sh
+++ entrypoint.sh
@@ -54,15 +54,16 @@ if [ ! -z "$8" ]; then
 fi
 
 MENDER_ARTIFACT_VERSION=$9
-curl https://downloads.mender.io/mender-artifact/${MENDER_ARTIFACT_VERSION}/linux/mender-artifact --output mender-artifact
+curl https://downloads.mender.io/mender-artifact/${MENDER_ARTIFACT_VERSION}/linux/mender-artifact --output ./mender-artifact
 chmod a+x mender-artifact
 
 echo $MENDER_ARTIFACT_VERSION
 
-ls -lah $ARTIFACT_CONTENT/
+ls -lah 
 echo "START"
+
 #mender-artifact write module-image -T "${TYPE}" -n "${ARTIFACT_NAME}" -t "${DEVICE_TYPE}" -o "${OUTPUT_PATH}" -f $(echo "$PACKAGES" | sed -e 's/ / -f /g')
 #mender-artifact write module-image -T "deb" -n "test" -t "raspberrypi3" -o "myupdate.mender" -f "content/nano_3.2-3_armhf.deb" | xargs echo -n
-echo -n `./mender-artifact write module-image`
-./mender-artifact write module-image | xargs echo -n
-echo "END"
+#echo -n `./mender-artifact write module-image`
+#./mender-artifact write module-image | xargs echo -n
+#echo "END"
