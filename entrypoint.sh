4171a9335902c130ad9dd1c166165830a4f24e57
KevinR <kvn.rhn@gmail.com>
2021-12-07 17:20:47 +0100

    test mender artifact

--- entrypoint.sh
+++ entrypoint.sh
@@ -60,5 +60,8 @@ done
 
 echo "$PACKAGES" | sed -e 's/ / -f /g'
 echo "HELLO"
+
+ls -lah $ARTIFACT_CONTENT/
+ls -lah ./
 #mender-artifact write module-image -T "${TYPE}" -n "${ARTIFACT_NAME}" -t "${DEVICE_TYPE}" -o "${OUTPUT_PATH}" -f $(echo "$PACKAGES" | sed -e 's/ / -f /g')
-mender-artifact write module-image -T deb -n test -t raspberrypi3 -o myupdate.mender -f content/nano_3.2-3_armhf.deb
+mender-artifact write module-image -T "deb" -n "test" -t "raspberrypi3" -o "myupdate.mender" -f "content/nano_3.2-3_armhf.deb"
